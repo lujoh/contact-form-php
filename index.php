9c80db444e405984e474ac6a1b517f467fb27c23
lujoh <lucyjohnson1995@gmail.com>
2020-12-16 17:28:27 -0500

    Added index.php file that will host the contact form

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>
+            Contact form index file
+        </title>
+        <meta charset="UTF-8">
+        <meta name="author" content="lujoh">
+    </head>
+    <body>
+        <h1>
+            Example page with contact form
+        </h1>
+        <p>
+            Contact form will be displayed below this paragraph.
+        </p>
+        <?php 
+        require_once "contact_form.php";
+            ?>
+    </body>
+</html>
