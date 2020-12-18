28cd55894674505474322414f89aa365bccab02b
lujoh <lucyjohnson1995@gmail.com>
2020-12-18 17:15:35 -0500

    added comments

--- _owner_settings_contact_form.php
+++ _owner_settings_contact_form.php
@@ -8,6 +8,11 @@ class Owner_settings {
     //The subject that the user enters in the form is added after the default subject part
     protected $website_name = "Sample Website"; //Change this to the name of your website
 }
-$base_color="#FFFFFF";
-$highlight_color="#000080";
-$main_color="#ccccff";
+$base_color="#FFFFFF"; //change this to change the base color. default is #FFFFFF aka white. 
+//this color is used for the background
+
+$highlight_color="#000080"; //change this to change the highlight color. default is #000080 aka dark blue. 
+//this color is used for the borders around the fields and buttons
+
+$main_color="#ccccff"; //change this to change the base color. default is #ccccff aka pale blue. 
+//this color is used for the background of the submit button and fields in focus
--- contact_form_style.php
+++ contact_form_style.php
@@ -2,25 +2,30 @@
 header("Content-type: text/css; charset: UTF-8"); //sets the file as a css file
 require_once '_owner_settings_contact_form.php';
 ?>
+/*style of the main container*/
 .contact_form_container {
     width:100%;
     background-color:<?php echo $base_color; ?>;
 }
+/*style of the input fields*/
 .contact_form_container input[type=text], .contact_form_container input[type=email], .contact_form_container textarea{
     width:70%;
     border:solid 2px <?php echo $highlight_color; ?>;
 }
+/*style of the submit button*/
 .contact_form_container input[type=submit]{
     border:solid 2px <?php echo $highlight_color; ?>;
     color:<?php echo $highlight_color; ?>;
     background-color:<?php echo $main_color; ?>;
     font-weight:bolder;
 }
+/*general style for all the input*/
 .contact_form_container input, .contact_form_container textarea{
     padding:1%;
     border-radius:4px;
     margin-bottom:1%;
 }
+/*style for the input fields when they are in focus*/
 input:focus, textarea:focus{
     background-color:<?php echo $main_color; ?>;
 }
